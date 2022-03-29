e516800ce4ed4da60b34cdd08dd9ed28f5d0a1af
4537648 <49045557+4537648@users.noreply.github.com>
2022-03-29 17:01:47 +0300

    External import static erased.

--- src/main/java/ru/gpbtest/distance/DistanceChecker.java
+++ src/main/java/ru/gpbtest/distance/DistanceChecker.java
@@ -1,12 +1,11 @@
 package ru.gpbtest.distance;
 
-import static java.lang.Math.sqrt;
 import static ru.gpbtest.distance.Common.*;
 
 public class DistanceChecker {
   public static void main(String[] args) {
-    Point p1 = new Point((double)getRandomNumber(1,100), (double)getRandomNumber(1,100));
-    Point p2 = new Point((double)getRandomNumber(1,100), (double)getRandomNumber(1,100));
+    Point p1 = new Point(getRandomNumber(1,100), getRandomNumber(1,100));
+    Point p2 = new Point(getRandomNumber(1,100), getRandomNumber(1,100));
     System.out.println("Point One is (X, Y): " + p1.x + ", " + p1.y);
     System.out.println("Point Two is (X, Y): " + p2.x + ", " + p2.y);
     System.out.println("Distance between points (main-class):  " + String.format("%.2f", distance(p1, p2)) );
@@ -14,7 +13,7 @@ public class DistanceChecker {
   }
 
   public static double distance(Point p1, Point p2) {
-    return sqrt(getSquared(p1.x - p2.x) + getSquared(p1.y - p2.y));
+    return Math.sqrt(getSquared(p1.x - p2.x) + getSquared(p1.y - p2.y));
   }
 
 }
--- src/main/java/ru/gpbtest/distance/Point.java
+++ src/main/java/ru/gpbtest/distance/Point.java
@@ -1,6 +1,5 @@
 package ru.gpbtest.distance;
 
-import static java.lang.Math.sqrt;
 import static ru.gpbtest.distance.Common.getSquared;
 
 public class Point {
@@ -14,6 +13,6 @@ public class Point {
   }
 
   public double distance(Point p2) {
-    return sqrt(getSquared(this.x - p2.x) + getSquared(this.y - p2.y));
+    return Math.sqrt(getSquared(this.x - p2.x) + getSquared(this.y - p2.y));
   }
 }
